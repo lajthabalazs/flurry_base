fecea361e450a2815037a4394abd191ef016231a
Lajtha András Balázs <lajthabalazs@yahoo.com>
2013-12-07 19:11:47 +0100

    Updated to v 3.3.0

--- android_project/gen/hu/droidium/flurrybase/R.java
+++ android_project/gen/hu/droidium/flurrybase/R.java
@@ -11,23 +11,21 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int bottomBanner=0x7f060001;
-        public static final int topBanner=0x7f060000;
+        public static int bottomBanner=0x7f060002;
+        public static int showFullScreenAd=0x7f060001;
+        public static int topBanner=0x7f060000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int flurryAdSpaceNameBottom=0x7f040003;
-        public static final int flurryAdSpaceNameFull=0x7f040004;
-        public static final int flurryAdSpaceNameTop=0x7f040002;
+        public static int app_name=0x7f040000;
         /**  Flurry stuff  
          */
-        public static final int flurryKey=0x7f040001;
+        public static int flurryKeys=0x7f040001;
     }
     public static final class style {
         /** 
@@ -51,10 +49,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static int AppTheme=0x7f050001;
     }
 }
--- android_project/src/hu/droidium/flurry_base/FlurryBaseActivity.java
+++ android_project/src/hu/droidium/flurry_base/FlurryBaseActivity.java
@@ -5,63 +5,87 @@ import java.util.Map;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.util.Log;
-
-import com.flurry.android.FlurryAds;
 import com.flurry.android.FlurryAgent;
 
-public class FlurryBaseActivity extends Activity {
+public abstract class FlurryBaseActivity extends Activity {
 	
 	private static final String FLURRY_PREFS = "Flurry preferences";
 	private static final String FLURRY_USER_ID_KEY = "Flurry user id";
-	private static final String FLURRY_KEY = "NGHFGDNC4VTBYFW8G2Y3";
+	private static final String FLURRY_LOGGING_ENABLED = "Logging enabled";
+	private static final String TAG = FlurryBaseActivity.class.getName();
 	
 	private SharedPreferences prefs;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		prefs = getSharedPreferences(FLURRY_PREFS, Context.MODE_PRIVATE);
+	}
+	
 	@Override
 	protected void onStart() {
 		super.onStart();
-		prefs = getSharedPreferences(FLURRY_PREFS, Context.MODE_PRIVATE);
 		long userId = prefs.getLong(FLURRY_USER_ID_KEY, -1);
 		if (userId == -1) {
 			prefs.edit().putLong(FLURRY_USER_ID_KEY, (long)(Math.random() * 1000000000)).commit();	
 		}
 		FlurryAgent.setUserId("" + userId);
-		Log.i("FlurrySession", "Starting session");
-		FlurryAgent.onStartSession(this, FLURRY_KEY);
-		FlurryAds.enableTestAds(true);
+		Log.i(TAG, "Starting session");
+		FlurryAgent.onStartSession(this, getFlurryKey());
 	}
 	
 
+	protected abstract String getFlurryKey();
+	protected boolean isLoggingEnabled() {
+		return prefs.getBoolean(FLURRY_LOGGING_ENABLED, false);
+	}
+	
+	protected boolean isLoggingEnabledSelected() {
+		return prefs.contains(FLURRY_LOGGING_ENABLED);
+	}
+	
+	protected void setLoggingEnabled(boolean eanbled) {
+		prefs.edit().putBoolean(FLURRY_LOGGING_ENABLED, eanbled).commit();
+	}
+
 	/* *********************** HELPER LOG FUNCTIONS ************************ */
-	protected void log(int event) {
+	public void log(int event) {
 		log(getString(event));
 	}
 
-	protected void log(int event, Map<String, String> params) {
+	public void log(int event, Map<String, String> params) {
 		log(getString(event), params, false);
 	}
 
-	protected void log(String event, Map<String, String> params) {
+	public void log(String event, Map<String, String> params) {
 		log(event, params, false);
 	}
 	
 	/* ************************ REAL LOG FUNCTIONS ************************ */
-	protected void log(String event) {
-		FlurryAgent.logEvent(event);
+	public void log(String event) {
+		if (isLoggingEnabled()) {
+			FlurryAgent.logEvent(event);
+		}
 	}
 
-	private void log(String event, Map<String, String> params, boolean timed) {
-		FlurryAgent.logEvent(event, params, timed);
+	public void log(String event, Map<String, String> params, boolean timed) {
+		if (isLoggingEnabled()) {
+			FlurryAgent.logEvent(event, params, timed);
+		}
 	}
 
-	protected void logEndTimedEvent(String event) {
-		FlurryAgent.endTimedEvent(event);
+	public void logEndTimedEvent(String event) {
+		if (isLoggingEnabled()) {
+			FlurryAgent.endTimedEvent(event);
+		}
 	}
 
 	@Override
 	protected void onStop() {
 		FlurryAgent.onEndSession(this);
-		Log.i("FlurrySession", "Session ended");
+		Log.i(TAG, "Session ended");
 		super.onStop();
 	}
 }
